07027eaddba176250d3b652e2a8a18246d4a6311
roon-replica <think-roon@naver.com>
2024-01-30 23:08:59 +0900

    프로젝트 생성

---
+++ src/test/java/roon/learn/auth/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package roon.learn.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
